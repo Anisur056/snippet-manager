c25160401719dc5366359c5ecb145e29eb5d2933
Anisur Rahman <ranisur056@gmail.com>
2025-07-10 15:40:16 +0600

    Create firewall_block.bat

---
+++ firewall-snippet/firewall_block.bat
@@ -0,0 +1,7 @@
+the code below is the batch script
+
+netsh advfirewall firewall add rule name="=====give a name of rule=====" dir=out program="====select the path of the application that you want to block====" action=block
+
+just add a rule name and application path.
+
+DONE....
